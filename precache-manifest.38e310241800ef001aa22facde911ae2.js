0b4b8435b914eedaeec3004b1dc7d224b99590ac
Chirag Rupani <36813124+chiragrupani@users.noreply.github.com>
2020-04-11 14:30:15 +0530

    Updates

---
+++ precache-manifest.38e310241800ef001aa22facde911ae2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b5fc84441ddec0220a4d840a35dc1d",
+    "url": "/ReactTSApp/index.html"
+  },
+  {
+    "revision": "fc9ae40f655b836c269b",
+    "url": "/ReactTSApp/static/css/main.d5bcbade.chunk.css"
+  },
+  {
+    "revision": "97cc8783dcdf2a65c987",
+    "url": "/ReactTSApp/static/js/2.bee9e842.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTSApp/static/js/2.bee9e842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc9ae40f655b836c269b",
+    "url": "/ReactTSApp/static/js/main.2b07e898.chunk.js"
+  },
+  {
+    "revision": "a24333ced084b1ae93f3",
+    "url": "/ReactTSApp/static/js/runtime-main.9bad2387.js"
+  }
+]);
